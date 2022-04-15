31f682fd5b205bf44e5eb2013849eb0b2383b167
Crypticsy <animesh19basnet@gmail.com>
2022-04-15 11:37:10 +0545

    tk-4 : rebase conflict fixes

---
+++ Data/ApplicaitonDbInitializer.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Identity;
+using RopeyDVDManagementSystem.Models.Identity;
+
+namespace RopeyDVDManagementSystem.Data
+{
+    public class ApplicaitonDbInitializer
+    {
+
+        public static async Task SeedUsersAndRolesAsync(IApplicationBuilder applicationBuilder)
+        {
+            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+                // Seeding roles to the initial database
+                var roleManager = serviceScope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+                if (!await roleManager.RoleExistsAsync(UserRoles.Admin))
+                    await roleManager.CreateAsync(new IdentityRole(UserRoles.Admin));
+
+                if (!await roleManager.RoleExistsAsync(UserRoles.User))
+                    await roleManager.CreateAsync(new IdentityRole(UserRoles.User));
+
+
+                // Seeding users to the initial database
+                var userManager = serviceScope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+                
+                string adminUserEmail = "admin@ropey.com";
+                var adminUser = await userManager.FindByEmailAsync(adminUserEmail);
+                if (adminUser == null)
+                {
+                    var newAdminUser = new IdentityUser()
+                    {
+                        UserName = "Admin-User",
+                        Email = adminUserEmail,
+                        SecurityStamp = Guid.NewGuid().ToString(),
+                        EmailConfirmed = true
+                    };
+                    await userManager.CreateAsync(newAdminUser, "Admin!123");
+                    await userManager.AddToRoleAsync(newAdminUser, UserRoles.Admin);
+                }
+
+                string appUserEmail = "user@ropey.com";
+                var appUser = await userManager.FindByEmailAsync(appUserEmail);
+                if (appUser == null)
+                {
+                    var newAppUser = new IdentityUser()
+                    {
+                        UserName = "App-User",
+                        Email = appUserEmail,
+                        SecurityStamp = Guid.NewGuid().ToString(),
+                        EmailConfirmed = true
+                    };
+                    await userManager.CreateAsync(newAppUser, "User!123");
+                    await userManager.AddToRoleAsync(newAppUser, UserRoles.User);
+                }
+
+            }
+        }
+
+    }
+}
--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,10 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using RopeyDVDManagementSystem.Models;
 
 namespace RopeyDVDManagementSystem.Data
 {
-    public class ApplicationDbContext : DbContext
+
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
 
@@ -12,7 +16,7 @@ namespace RopeyDVDManagementSystem.Data
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            
+
             builder.Entity<CastMember>().HasKey(cm => new
             {
                 cm.ActorNumber,
@@ -21,6 +25,7 @@ namespace RopeyDVDManagementSystem.Data
 
             builder.Entity<CastMember>().HasOne(cm => cm.Actor).WithMany(cm => cm.CastMembers).HasForeignKey(m => m.ActorNumber);
             builder.Entity<CastMember>().HasOne(cm => cm.DVDTitle).WithMany(cm => cm.CastMembers).HasForeignKey(m => m.DVDNumber);
+
             base.OnModelCreating(builder);
         }
     }
