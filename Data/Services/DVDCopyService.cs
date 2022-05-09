5698890a81ead98bdf0827fcc5295f0fffc69b12
Pranamya <pranamyasharma94@gmail.com>
2022-05-09 12:36:56 +0545

    Added comments

--- Data/Services/DVDCopyService.cs
+++ Data/Services/DVDCopyService.cs
@@ -10,27 +10,27 @@ namespace RopeyDVDManagementSystem.Data.Services
         {
             _context = context;
         }
-        public async Task AddAsync(DVDCopy dvdcopy)
+        public async Task AddAsync(DVDCopy dvdcopy) //To add a new DVD copy
         {
             await _context.DVDCopies.AddAsync(dvdcopy);
             await _context.SaveChangesAsync();
         }
 
-        public async Task DeleteAsync(int id)
+        public async Task DeleteAsync(int id) //To delete an existing DVD copy according to its CopyNumber
         {
             var result = await _context.DVDCopies.FirstOrDefaultAsync(n => n.CopyNumber == id);
             _context.DVDCopies.Remove(result);
             await _context.SaveChangesAsync();
         }
 
-        public async Task<IEnumerable<DVDCopy>> GetAllAsync()
+        public async Task<IEnumerable<DVDCopy>> GetAllAsync() //Get all copy information for the index
         {
             var result = await _context.DVDCopies.ToListAsync();
             return result;
         }
 
 
-        public async Task<DVDCopy> UpdateAsync(int id, DVDCopy newDVDCopy)
+        public async Task<DVDCopy> UpdateAsync(int id, DVDCopy newDVDCopy) //Edit an existing DVD copy according to its CopyNumber
         {
             _context.Update(newDVDCopy);
             await _context.SaveChangesAsync();
@@ -38,7 +38,7 @@ namespace RopeyDVDManagementSystem.Data.Services
 
         }
 
-        public async Task<DVDCopy> GetByIdAsync(int id)
+        public async Task<DVDCopy> GetByIdAsync(int id) //Get the details of a particular DVD copy for viewing details/deleting
         {
             var result = await _context.DVDCopies.FirstOrDefaultAsync(n => n.CopyNumber == id);
             return result;
